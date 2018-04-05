1cd408ac83881513c4261100ac848f045d5876df
Sascha Hunold <sascha@hunoldscience.net>
2018-04-05 13:29:46 +0200

    added c99 flag to compiler options

--- platform_files/default.cmake
+++ platform_files/default.cmake
@@ -4,7 +4,7 @@ find_package(MPI REQUIRED)
 
 SET(CMAKE_C_COMPILER mpicc)
 SET(CMAKE_CXX_COMPILER mpicxx)
-SET(CMAKE_C_FLAGS "-O3 -Wall ") # -pedantic")
+SET(CMAKE_C_FLAGS "-O3 -Wall -std=c99 -D_POSIX_C_SOURCE=200809L") # -pedantic")
 
 SET(BUILD_SHARED_LIBS 1)
 
