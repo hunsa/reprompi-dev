41c4f39b92350ce4d2234f7ad506e27e64ee326b
Sascha Hunold <sascha@hunoldscience.net>
2018-05-08 15:22:08 +0200

    add error handling to synclib

---
+++ src/reprompi_bench/sync/clock_sync/utils/sync_errors.cpp
@@ -0,0 +1,17 @@
+
+#include <cstdio>
+#include <mpi.h>
+
+#include "sync_errors.hpp"
+
+void exit_on_sync_lib_error(const char* error_str) {
+  int my_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+  if (my_rank == 0) {
+    fprintf(stderr, "\nERROR: %s\n\n", error_str);
+  }
+  MPI_Finalize();
+  exit(1);
+}
+
---
+++ src/reprompi_bench/sync/clock_sync/utils/sync_errors.hpp
@@ -0,0 +1,15 @@
+
+#ifndef REPROMPI_BENCH_SYNC_CLOCK_SYNC_UTILS_SYNC_ERRORS_HPP_
+#define REPROMPI_BENCH_SYNC_CLOCK_SYNC_UTILS_SYNC_ERRORS_HPP_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void exit_on_sync_lib_error(const char* msg);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* REPROMPI_BENCH_SYNC_CLOCK_SYNC_UTILS_SYNC_ERRORS_HPP_ */
