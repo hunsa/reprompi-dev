4c4927f2a4a42d5077f1fa3922e1bbde469ff796
Sascha Hunold <sascha@hunoldscience.net>
2022-09-26 08:18:09 +0200

    extract subcode to share between mpibenchmark and pgchecker

--- src/pgcheck/pgtunelib_interface.cpp
+++ src/pgcheck/pgtunelib_interface.cpp
@@ -27,15 +27,42 @@ PGTuneLibInterface::PGTuneLibInterface(std::string pgmpi_info_str)
   std::stringstream csv_stream(pgmpi_info_str);
   CSVReader csv_reader(csv_stream, format);
 
-  for(auto& cname : csv_reader.get_col_names()) {
-    std::cout << "csv col name: " << cname << std::endl;
-  }
-
   for (auto& row: csv_reader) {
-    std::cout << "csv row: " << row["cliname"].get() << std::endl;
+    std::string mpiname = row["mpiname"].get();
+    if( mpi2module.count(mpiname) < 1 ) {
+      mpi2module[mpiname] = std::make_tuple( row["cliname"].get(), std::vector<std::string> { row["algname"].get() } );
+    } else {
+      std::get<1>(mpi2module[mpiname]).push_back(row["algname"].get());
+    }
   }
 
+}
 
+std::string PGTuneLibInterface::get_module_name_for_mpi_collectives(std::string mpi_coll_name) {
+  std::string mname = "";
+  if( mpi2module.count(mpi_coll_name) > 0 ) {
+    mname = std::get<0>(mpi2module[mpi_coll_name]);
+  }
+  return mname;
+}
+
+std::vector<std::string> PGTuneLibInterface::get_available_mpi_collectives() {
+  std::vector<std::string> mpi_colls;
+  for(auto& e : mpi2module) {
+    mpi_colls.push_back(e.first);
+  }
+  sort(mpi_colls.begin(), mpi_colls.end());
+  return mpi_colls;
+}
+
+std::vector<std::string> PGTuneLibInterface::get_available_implementations_for_mpi_collectives(std::string mpi_coll_name) {
+  std::vector<std::string> mpi_coll_versions;
+  if( mpi2module.count(mpi_coll_name) > 0 ) {
+    for(auto &v : std::get<1>(mpi2module[mpi_coll_name])) {
+      mpi_coll_versions.push_back(v);
+    }
+  }
+  return mpi_coll_versions;
 }
 
 
--- src/pgcheck/pgtunelib_interface.h
+++ src/pgcheck/pgtunelib_interface.h
@@ -6,12 +6,24 @@
 #define REPROMPI_SRC_PGCHECK_PGTUNELIB_INTERFACE_H
 
 #include <string>
+#include <vector>
+#include <unordered_map>
 
 class PGTuneLibInterface
 {
 
+private:
+  std::vector<std::string> mpi_collectives;
+  std::unordered_map<std::string, std::tuple<std::string, std::vector<std::string>>> mpi2module;
+
 public:
   PGTuneLibInterface(std::string pgmpi_info_str);
+
+  std::vector<std::string> get_available_mpi_collectives();
+
+  std::string get_module_name_for_mpi_collectives(std::string mpi_coll_name);
+
+  std::vector<std::string> get_available_implementations_for_mpi_collectives(std::string mpi_coll_name);
 
 };
 
