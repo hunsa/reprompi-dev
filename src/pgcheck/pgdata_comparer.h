a339d686a6a51b251b0a09e8ae68883e7919e6cf
Maximilian Hagn <maximilian.hagn@gmail.com>
2022-10-30 14:48:29 +0100

    deleted StatisticValues Struct

--- src/pgcheck/pgdata_comparer.h
+++ src/pgcheck/pgdata_comparer.h
@@ -11,32 +11,22 @@
 #include <iomanip>
 #include <numeric>
 
-struct StatisticValues {
-  int size;
-  double mean;
-  double median;
-  double variance;
-  std::string mockup;
-  double mockup_median;
-};
-
 class PGDataComparer {
 
 protected:
   std::string mpi_coll_name;
   int nnodes;  // number of nodes
   int ppn;     // number of processes per node
-//  const static double critical_t_values[];
-//  const static double normal_distribution_value;
-  std::unordered_map<std::string, PGData*> mockup2data;
+  std::unordered_map<std::string, PGData *> mockup2data;
 
 public:
 
   PGDataComparer(std::string mpi_coll_name, int nnodes, int ppn);
+
   void add_dataframe(std::string mockup_name, PGData *data);
+
   virtual std::string get_results() = 0;
 
 };
-
 
 #endif //REPROMPI_SRC_PGCHECK_PGDATA_COMPARER_H
