1d13eb729deed333883c7adeb3c7802a627c473a
Maximilian Hagn <maximilian.hagn@gmail.com>
2022-11-18 18:50:23 +0100

    refactored and documented statistics_utils.h

--- src/pgcheck/utils/statistics_utils.h
+++ src/pgcheck/utils/statistics_utils.h
@@ -5,7 +5,6 @@
 #ifndef REPROMPI_DEV_SRC_PGCHECK_UTILS_STATISTICS_UTILS_H
 #define REPROMPI_DEV_SRC_PGCHECK_UTILS_STATISTICS_UTILS_H
 
-#include <string>
 #include <vector>
 #include <cmath>
 #include <numeric>
@@ -15,8 +14,17 @@ class StatisticsUtils {
 
 public:
   StatisticsUtils();
+  /**
+   * @return mean of a vector in any numerical format
+   */
   T mean(std::vector <T> v);
+  /**
+   * @return median of a vector in any numerical format
+   */
   T median(std::vector <T> v);
+  /**
+   * @return variance of a vector in any numerical format
+   */
   T variance(std::vector <T> v);
 };
 
