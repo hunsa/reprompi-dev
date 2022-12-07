ec6a6dfe2e0bce7c3f340789b4cc4e8d5b0429c2
Maximilian Hagn <maximilian.hagn@gmail.com>
2022-12-07 18:18:56 +0100

    Optimized new comparer types

--- src/pgcheck/comparer/abs_runtime_comparer.cpp
+++ src/pgcheck/comparer/abs_runtime_comparer.cpp
@@ -8,34 +8,23 @@ AbsRuntimeComparer::AbsRuntimeComparer(std::string mpi_coll_name, int nnodes, in
     PGDataComparer(mpi_coll_name, nnodes, ppn) {}
 
 PGDataTable AbsRuntimeComparer::get_results() {
-  std::vector <std::string> col_names = {"message_size"};
-  std::vector<int> col_widths = {15};
+  std::vector <std::string> col_names = {"message_size", "mockup", "median_ms"};
+  std::vector<int> col_widths = {15, 50, 10};
   PGDataTable res(mpi_coll_name, col_names);
   StatisticsUtils<double> statisticsUtils;
-  bool first = true;
 
-  for (auto &count: mockup2data.at("default")->get_unique_counts()) {
-    std::unordered_map <std::string, std::string> row;
-    row["message_size"] = std::to_string(count);
-
-    for (auto &mdata: mockup2data) {
-      auto &data = mockup2data.at(mdata.first);
+  for (auto &mdata: mockup2data) {
+    auto &data = mockup2data.at(mdata.first);
+    for (auto &count: data->get_unique_counts()) {
       auto rts = data->get_runtimes_for_count(count);
-
-      if (first) {
-        col_names.push_back(data->get_mockup_name());
-        // 2 spaces padding on each side
-        col_widths.push_back(data->get_mockup_name().size() + 4);
-      }
-
-      row[data->get_mockup_name()] = std::to_string(statisticsUtils.median(rts) * 1000);
-
+      std::unordered_map <std::string, std::string> row;
+      row["message_size"] = std::to_string(count);
+      row["mockup"] = mdata.first;
+      row["median_ms"] = std::to_string(statisticsUtils.mean(rts) * 1000);
+      res.add_row(row);
     }
-    res.add_row(row);
-    first = false;
   }
 
-  res.set_col_names(col_names);
   res.set_col_widths(col_widths);
   return res;
 }
