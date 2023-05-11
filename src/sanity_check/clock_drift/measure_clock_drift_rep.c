571ac0c8f369597205938aa915455e332bed69c2
Sascha Hunold <sascha@hunoldscience.net>
2023-05-11 23:26:17 +0200

    even better output

--- src/sanity_check/clock_drift/measure_clock_drift_rep.c
+++ src/sanity_check/clock_drift/measure_clock_drift_rep.c
@@ -239,13 +239,13 @@ int main(int argc, char* argv[]) {
     f = stdout;
     if (my_rank == master_rank) {
 
-      fprintf(f,"%4s %5s %14s\n", "nrep", "p", "min_diff");
+      fprintf(f,"%4s %7s %14s\n", "nrep", "p", "min_diff");
 
       for(int i=0; i<opts.reps; i++) {
         for (index = 0; index < ntestprocs; index++) {
           p = testprocs_list[index];
           min_drift = all_global_times[i * ntestprocs + index];
-          fprintf(f, "%4d %5d %14.9f\n", i, p, fabs(min_drift));
+          fprintf(f, "%4d %7d %14.9f\n", i, p, fabs(min_drift));
         }
       }
 
